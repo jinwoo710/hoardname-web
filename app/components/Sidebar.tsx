a1d08111c6029a312678234e3badc1aaaa3bbd7a
jinwoo710 <jinwoo710@naver.com>
2025-01-03 18:51:05 +0900

    sidebar 수정

--- app/components/Sidebar.tsx
+++ app/components/Sidebar.tsx
@@ -1,5 +1,7 @@
+'use client';
 import Image from 'next/image';
 import Link from 'next/link';
+import { useSession } from "next-auth/react";
 
 interface SideBarItemProps {
     imageUrl: string;
@@ -26,12 +28,20 @@ interface SideBarProps {
 }
 
 export default function SideBar({ onClose }: SideBarProps) {
+    const { data: session } = useSession();
+
     return (
         <div className="w-full shrink-0 flex flex-col items-center p-4 space-y-2 ">
             <SideBarItem imageUrl="/search.svg" title="게임 리스트" href="/game" onClose={onClose}/>
             <SideBarItem imageUrl="/ticket.svg" title="중고 장터" href="/shop" onClose={onClose}/>
-            <SideBarItem imageUrl="/user.svg" title="회원 정보" href="/user" onClose={onClose}/>
-            <SideBarItem imageUrl="/lock.svg" title="관리자" href="/manager" onClose={onClose}/>
+            {session && (
+                <>
+                    <SideBarItem imageUrl="/user.svg" title="회원 정보" href="/user" onClose={onClose} />
+                    <SideBarItem imageUrl="/lock.svg" title="My 게임" href="/user-game" onClose={onClose}/>
+                     <SideBarItem imageUrl="/lock.svg" title="My 장터" href="/user-shop" onClose={onClose}/>
+           
+                </>
+            )}
         </div>
     )
 }
