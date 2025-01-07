2fd23af10ec82051c90a0e411f6bf250324137a3
jinwoo710 <jinwoo710@naver.com>
2025-01-07 17:55:43 +0900

    add email

---
+++ app/email/page.tsx
@@ -0,0 +1,13 @@
+export default async function Email() {
+  return (
+    <div className="text-center pt-[50px] lg:pt-[200px] text-lg text-gray-500">
+      작업중 ... 작업중 ... 작업중 ... 작업중 ... 작업중 ... 작업중 ... 작업중 ... 작업중 ... 작업중 ...
+      <br/>
+      업데이트 전까지 문의나 버그 및 피드백은 지누 갠톡주세요
+       <br/>
+      피드백 대 환영 합니다
+      <br />
+      디자인이라던가... 코드 측에서라던가... 아이디어 대환영...
+    </div>
+  )
+}
