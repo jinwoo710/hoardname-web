7b80df3f209a86ac15b677f5c32ac50d3d38d63e
jinwoo710 <32620222+jinwoo710@users.noreply.github.com>
2025-01-10 14:16:41 +0900

    Revert "카카오톡 웹뷰 제거"

--- app/layout.tsx
+++ app/layout.tsx
@@ -25,7 +25,6 @@ export default function RootLayout({
             <aside className="hidden lg:block lg:w-[220px] lg:flex-shrink-0">
               <SideBar />
             </aside>
-            
             <main className="flex-grow">
               {children}
             </main>
