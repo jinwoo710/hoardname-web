f6d81457f6800bbc115602e0074a0da519b4523e
jinwoo710 <jinwoo710@naver.com>
2025-01-13 14:29:59 +0900

    불필요한 잔재 제거

--- auth.ts
+++ auth.ts
@@ -10,21 +10,6 @@ export const { handlers, auth, signIn, signOut } = NextAuth({
     Google({
       clientId: process.env.GOOGLE_CLIENT_ID!,
       clientSecret: process.env.GOOGLE_CLIENT_SECRET!,
-      authorization: {
-        params: {
-          prompt: "consent",
-          access_type: "offline",
-          response_type: "code",
-        },
-      },
-      profile(profile) {
-        return {
-          id: profile.sub,
-          name: profile.name,
-          email: profile.email,
-          image: profile.picture,
-        };
-      },
     }),
   ],
   session: {
@@ -42,24 +27,6 @@ export const { handlers, auth, signIn, signOut } = NextAuth({
         return !!profile?.email_verified;
       }
       return true;
-    },
-    async redirect({ url, baseUrl }) {
-      try {
-        const urlParams = new URL(url).searchParams;
-        const state = urlParams.get("state");
-        if (state) {
-          const { returnUrl } = JSON.parse(decodeURIComponent(state));
-          if (returnUrl) {
-            return returnUrl;
-          }
-        }
-      } catch (error) {
-        console.error("Error parsing state:", error);
-      }
-
-      if (url.startsWith(baseUrl)) return url;
-      if (url.startsWith("/")) return new URL(url, baseUrl).toString();
-      return baseUrl;
     },
   },
 });
